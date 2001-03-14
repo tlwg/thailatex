abb1dd73ad0d5d3925cca0bed9aa0eaf3805b754
thep <thep@0f6cb6e5-e671-47ff-8166-28bcf753934f>
2001-03-14 12:30:21 +0000

    This commit was generated by cvs2svn to compensate for changes in r2, which included commits to RCS files with non-trunk default branches.

---
+++ trunk/dvips/addpsfont.sh
@@ -0,0 +1,15 @@
+if [ -f $DVIPSDIR/psfonts.map ] ; then
+  sed -e '/^dbtt/d' < $DVIPSDIR/psfonts.map \
+    | sed -e '/^rnorasi/d' > /tmp/tmp.$$
+  mv /tmp/tmp.$$ $DVIPSDIR/psfonts.map
+fi
+sed -e 's/^X//' >> $DVIPSDIR/psfonts.map << END
+Xdbtt   DBThaiText <dbtt.pfb "dbttEncoding ReEncodeFont" <dbttx.enc
+Xdbttb  DBThaiTextBold <dbttb.pfb "dbttEncoding ReEncodeFont" <dbttx.enc
+Xdbtti  DBThaiTextItalic <dbtti.pfb "dbttEncoding ReEncodeFont" <dbttx.enc
+Xdbttbi DBThaiTextBoldItalic <dbttbi.pfb "dbttEncoding ReEncodeFont" <dbttx.enc
+Xrnorasi_n  Norasi <norasi_n.pfb "TeXTISEncoding ReEncodeFont" <TeXTIS.enc
+Xrnorasi_b  Norasi-Bold <norasi_b.pfb "TeXTISEncoding ReEncodeFont" <TeXTIS.enc
+Xrnorasi_i  Norasi-Italic <norasi_i.pfb "TeXTISEncoding ReEncodeFont" <TeXTIS.enc
+Xrnorasi_bi Norasi-BoldItalic <norasi_bi.pfb "TeXTISEncoding ReEncodeFont" <TeXTIS.enc
+END
