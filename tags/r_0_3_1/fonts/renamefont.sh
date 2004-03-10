14999ebf143c1df17b02d5c823bcd897d9028829
nobody <nobody@0f6cb6e5-e671-47ff-8166-28bcf753934f>
2004-03-10 01:19:19 +0000

    This commit was manufactured by cvs2svn to create tag 'r_0_3_1'.

---
+++ tags/r_0_3_1/fonts/renamefont.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+MMV=`which mmv`
+
+if [ x$MMV == x ]
+then
+	echo "I need mmv. Please apt-get install mmv"
+	exit 1
+fi
+
+#Norasi, Garuda, and DBThaiText
+
+$MMV -v "Norasi*" "norasi#1"
+$MMV -v "Garuda*" "garuda#1"
+$MMV -v "DBThaiText*" "dbtt#1"
+$MMV -v "Kinnari*" "kinnari#1"
+
+#Oblique, Bold, BoldOblique, Italic, BoldItalic
+$MMV -v "*-BoldOblique*" "#1_bo#2"
+$MMV -v "*-BoldItalic*" "#1_bi#2"
+$MMV -v "*-Bold*" "#1_b#2"
+$MMV -v "*-Oblique*" "#1_o#2"
+$MMV -v "*-Italic*" "#1_i#2"
