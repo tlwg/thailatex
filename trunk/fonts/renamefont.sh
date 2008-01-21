4b0b9a18665b3a70a82858af022fa587e5c4a311
thep <thep@0f6cb6e5-e671-47ff-8166-28bcf753934f>
2008-01-21 10:51:51 +0000

    * fonts/renamefont.sh: Add renaming for Waree, Umpush, Sawasdee.

--- trunk/fonts/renamefont.sh
+++ trunk/fonts/renamefont.sh
@@ -18,6 +18,9 @@ $MMV -v "TlwgTypewriter*" "ttype#1"
 $MMV -v "TlwgTypist*" "ttypist#1"
 $MMV -v "Purisa*" "purisa#1"
 $MMV -v "Loma*" "loma#1"
+$MMV -v "Waree*" "waree#1"
+$MMV -v "Umpush*" "umpush#1"
+$MMV -v "Sawasdee*" "sawasdee#1"
 
 #Oblique, Bold, BoldOblique, Italic, BoldItalic
 $MMV -v "*-BoldOblique*" "#1_bo#2"
@@ -25,3 +28,8 @@ $MMV -v "*-BoldItalic*" "#1_bi#2"
 $MMV -v "*-Bold*" "#1_b#2"
 $MMV -v "*-Oblique*" "#1_o#2"
 $MMV -v "*-Italic*" "#1_i#2"
+
+# for Sawasdee, whose names don't include hyphen
+$MMV -v "*BoldOblique*" "#1_bo#2"
+$MMV -v "*Bold*" "#1_b#2"
+$MMV -v "*Oblique*" "#1_o#2"
